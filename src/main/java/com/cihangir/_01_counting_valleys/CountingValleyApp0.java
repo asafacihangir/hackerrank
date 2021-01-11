14d4e3b132d50b055044ba59c5239b716f5e0769
asefacihangir <sefa.cihangir@iottech.com.tr>
2021-01-11 22:21:23 +0300

    counting valleys implemented

---
+++ src/main/java/com/cihangir/_01_counting_valleys/CountingValleyApp0.java
@@ -0,0 +1,51 @@
+package com.cihangir._01_counting_valleys;
+
+import java.io.IOException;
+
+//my solution
+public class CountingValleyApp0 {
+
+    public static void main(String[] args) throws IOException {
+
+        int n = 12;
+        String path = "DDUUDDUDUUUD";
+        int result = countingValleys(n, path);
+
+        System.out.println(result);
+    }
+
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+    public static int countingValleys(int steps, String path) {
+
+        int valleyCount = 0;
+        int previousPosition;
+        int currentPosition = 0;
+
+        for (int i = 0; i < steps; i++) {
+            previousPosition = currentPosition;
+            char step = path.charAt(i);
+            boolean up = step == 'U';
+
+            if ( step == 'U') {
+                currentPosition++;
+            }else {
+                currentPosition--;
+            }
+            if ( i !=0 && previousPosition <0 && currentPosition == 0){
+                valleyCount++;
+            }
+        }
+
+        return valleyCount;
+    }
+
+
+}
---
+++ src/main/java/com/cihangir/_01_counting_valleys/countingvalleys_solution.md
@@ -0,0 +1,5 @@
+https://www.hackerrank.com/challenges/counting-valleys/problem
+# Counting Valleys Solution
+Our goal is to count the number of valleys. A valley is a sequence of steps starting with a step downward from sea level and ending with a step upward to sea level. Let **level**  be a variable denoting the current altitude. If we take a step upwards,  **level** is incremented by one; if we take step downwards, **level**  is decremented by one.
+
+Since we know that the sequence of input steps starts and ends at sea level, then we can say that our  **level** variable is **0** at the beginning and end of the hike. The number of valleys can be counted as the number of steps taken upwards to sea level (i.e., when  **level**  goes from **-1**  to **0**. This is true, because each such step ends the sequence of steps below sea level, signifying the end of a valley.
