dc4a77d817a2825c7b53187e62caa0c94f6c579d
asefacihangir <sefa.cihangir@iottech.com.tr>
2022-03-13 12:33:11 +0300

    max array sum added

---
+++ src/main/java/com/cihangir/_01_maxarraysum/MaxArraySumApp0.java
@@ -0,0 +1,48 @@
+package com.cihangir._01_maxarraysum;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MaxArraySumApp0 {
+
+  // Complete the maxSubsetSum function below.
+  static int maxSubsetSum(int[] arr) {
+
+    int length = arr.length;
+    if (length == 0) return 0;
+    if (length == 1) return Math.max(0, arr[0]);
+
+    int[] dp = new int[length];
+
+    dp[0] = Math.max(0, arr[0]);
+    dp[1] = Math.max(dp[0], arr[1]);
+
+    for (int i = 2; i < length; i++) {
+      dp[i] = Math.max(dp[i - 1], arr[i] + dp[i - 2]);
+    }
+
+    return dp[length - 1];
+  }
+
+
+
+  public static void main(String[] args) throws IOException {
+    final String currentDirectory = System.getProperty("user.dir");
+    final String inputPath = currentDirectory + "/src/main/resources/01/input2.txt";
+
+    BufferedReader bufferedReader = new BufferedReader(new FileReader(inputPath));
+
+    int arraySize = Integer.parseInt(bufferedReader.readLine());
+    String[] arrItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+    int[] arr = new int[arraySize];
+    for (int i = 0; i < arraySize; i++) {
+      int arrItem = Integer.parseInt(arrItems[i]);
+      arr[i] = arrItem;
+    }
+
+    int res = maxSubsetSum(arr);
+    System.out.println(res);
+  }
+}
